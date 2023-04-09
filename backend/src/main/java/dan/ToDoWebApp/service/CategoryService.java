ac80a0e92d43a6109e5d121803822604fc85885d
cardosdan <cardosdan08@gmail.com>
2023-04-09 21:04:06 +0300

    Update

--- backend/src/main/java/dan/ToDoWebApp/service/CategoryService.java
+++ backend/src/main/java/dan/ToDoWebApp/service/CategoryService.java
@@ -3,7 +3,6 @@ package dan.ToDoWebApp.service;
 import dan.ToDoWebApp.model.Category;
 import dan.ToDoWebApp.model.Task;
 import dan.ToDoWebApp.repository.CategoryRepository;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
@@ -13,7 +12,8 @@ public class CategoryService {
     @Autowired private CategoryRepository categoryRepository;
     @Autowired private MongoTemplate mongoTemplate;
 
-    public Category createCategory(String name, ObjectId id) {
+
+    public Category createCategory(String name, String id) {
         Category category = categoryRepository.insert(new Category(name));
 
         mongoTemplate.update(Task.class)
@@ -27,17 +27,17 @@ public class CategoryService {
         return categoryRepository.findAll();
     }
 
-    public void deleteCategory(ObjectId id) {
+    public void deleteCategory(String id) {
         categoryRepository.deleteById(id);
     }
 
-    public void updateCategory(ObjectId id, String name) {
+    public void updateCategory(String id, String name) {
         Category category = categoryRepository.findById(id).get();
         category.setName(name);
         categoryRepository.save(category);
     }
 
-    public Category getCategory(ObjectId id) {
+    public Category getCategory(String id) {
         return categoryRepository.findById(id).get();
     }
 }
